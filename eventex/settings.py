d532a7e76df0ef0a257b1cc50b357b4a2ff46c3b
Rafael Marques <rafamqrs@gmail.com>
2013-09-19 16:59:46 -0300

    Debug false

--- eventex/settings.py
+++ eventex/settings.py
@@ -1,8 +1,9 @@
 # Django settings for eventex project.
 from unipath import Path
+import os
 import dj_database_url
 PROJECT_DIR = Path(__file__).parent
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'TRUE'
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -19,7 +20,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', 'herokuapp.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -134,6 +135,9 @@ LOGGING = {
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue'
         }
     },
     'handlers': {
@@ -141,11 +145,16 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'console':{
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
     },
     'loggers': {
         'django.request': {
-            'handlers': ['mail_admins'],
+            'handlers': ['mail_admins', 'console'],
             'level': 'ERROR',
             'propagate': True,
         },
